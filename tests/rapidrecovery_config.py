3221d0f3398d74166d996444cf5ea7d694e3e456
mbugaiov <maxim.bugaiov@softheme.com>
2018-01-03 12:53:44 +0200

    Fixed test for the snapper for the SLES 12 SP 2

--- tests/rapidrecovery_config.py
+++ tests/rapidrecovery_config.py
@@ -39,16 +39,40 @@ class RapidrecoveryConfig(Agent):
                 if self.execute.error_code(cmd='sudo /sbin/rcSuSEfirewall2 status') is 3: #check that firewall is disabled
                     self.file_exists(result=True, file=config)
                     self.file_exists(result=False, file=configuration_log)
-                    self.rapidrecovery_config_api(port="8006", user="vagrant", build="all", snapper="disable", start=True)
+                    if self.execute.error_code(cmd='snapper list') is 1:
+                        self.rapidrecovery_config_api(port="8006",
+                                                      user="vagrant",
+                                                      build="all",
+                                                      start=True)
+                        if self.execute.error_code(cmd='rapidreocovery-config -n disable') is not 1:
+                            return Exception('There should be 1 error code for the snapper, when it is disabled.'
+                                             'Got %s' %self.execute.error_code(cmd='rapidreocovery-config -n disable'))
+                    else:
+                        self.rapidrecovery_config_api(port="8006", user="vagrant", build="all", snapper="disable", start=True)
                 else:
                     firewall = "lokkit"
                     self.file_exists(result=True, file=config)
                     self.file_exists(result=False, file=configuration_log)
-                    self.rapidrecovery_config_api(port="8006", user="vagrant", method=firewall,
-                                              build="all", snapper="disable", start=True)
+                    if self.execute.error_code(cmd='snapper list') is 1:
+                        self.rapidrecovery_config_api(port="8006",
+                                                      user="vagrant",
+                                                      method=firewall,
+                                                      build="all",
+                                                      start=True)
+                        if self.execute.error_code(
+                                cmd='rapidreocovery-config -n disable') is not 1:
+                            return Exception('There should be 1 error code for the snapper, when it is disabled.'
+                                             'Got %s' % self.execute.error_code(cmd='rapidreocovery-config -n disable'))
+                    else:
+                        self.rapidrecovery_config_api(port="8006",
+                                                      user="vagrant",
+                                                      method=firewall,
+                                                      build="all",
+                                                      snapper="disable",
+                                                      start=True)
 
             else:
-                return Exception("The is error in the os determination/")
+                return Exception("The is error in the os determination.")
 
         except Exception as E:
             print(E)
